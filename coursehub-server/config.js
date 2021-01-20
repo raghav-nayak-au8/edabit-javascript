29cee7550ab23f9b08358da6357d904909195b31
Raghav Nayak <raghavnayak0488@gmail.com>
2021-01-20 13:51:19 +0530

    course hub project client and server

---
+++ coursehub-server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    hostUrl: 'http://localhost:5000',
+    clientUrl: 'http://localhost:3000',
+    database: 'mongodb+srv://dbUser:dbUserPass@cluster0.dj4gz.mongodb.net/iStudy?retryWrites=true&w=majority',
+    secret: "$S$E$C$R$E$T$",
+    port: 5000,
+    INITDB: true,
+};
